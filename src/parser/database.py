f4a5f9685133f2857d67964383e5bd943044517d
Ekaterina Samarkina <sea94@inbox.ru>
2020-05-19 20:54:19 +0400

    Add creation of the report

--- src/parser/database.py
+++ src/parser/database.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 
 def touch(path):
     with open(path, 'a'):
@@ -14,24 +15,38 @@ def create_dir(path):
     if not os.path.exists(basedir):
         os.makedirs(basedir)
 
+def remover_temps():
+    dir_path = "./src/parser/resources/temp/"
+    try:
+        shutil.rmtree(dir_path)
+    except OSError as e:
+        print("Error: %s : %s" % (dir_path, e.strerror))
+
+    logging.info('All temp files was removed')
+
 def create_database():
-    logging.info('********* STEP 1. Create json-DB if it is not exist *********')
+    logging.info('********* STEP 1. Create json-DB (articles, authors and report) if it is not exist *********')
     filenames = []
 
     filenameArt = "articles"
     filenameAut = "authors"
+    filenameRep = "report"
+
     filenames.append("./src/parser/resources/" + filenameArt + ".json")
     filenames.append("./src/parser/resources/temp/" + filenameArt + "_temp.json")
+
     filenames.append("./src/parser/resources/" + filenameAut + ".json")
     filenames.append("./src/parser/resources/temp/" + filenameAut + "_temp.json")
+
+    filenames.append("./src/parser/resources/" + filenameRep + ".json")
 
     dirs = []
     dirs.append("./src/parser/resources/")
     dirs.append("./src/parser/resources/temp/")
+    dirs.append("./src/parser/resources/img/")
 
     for dir in dirs:
         create_dir(dir)
 
     for filename in filenames:
         if_not_exist(filename)
-
--- src/parser/report.py
+++ src/parser/report.py
@@ -2,6 +2,68 @@ import src.parser.crawler as crawler
 import logging
 import os
 import database
+import pandas as pd
+import numpy as np
+import functions as func
+import matplotlib.pyplot as plt
+import json
+
+def authors_report(filenameDBAuth):
+    # 1) Top-5 Authors (based on articles counter)
+    authorsData = func.json_reader(filenameDBAuth)
+    authorsDF = pd.DataFrame(data=authorsData)
+    authorsDFSorted = authorsDF.sort_values(by=['counterArticles'], ascending=False)[0:5]
+    return authorsDFSorted
+
+def articles_report(filenameDBArt):
+    # 2) Top-5 New Articles (based on publish data)
+    articlesData = func.json_reader(filenameDBArt)
+    articleDFSorted = pd.DataFrame(data=articlesData)[-5:]
+    return articleDFSorted
+
+def tags_report(filenameDBArt):
+    # 3) Plot with counts of 7 popular tags:
+    #  - it must be a bar chart (column plot) where each column is for one tag
+    #  - Tag bar must have name in plot.
+    #  - X-axis - counter with articles of tag theme
+    articlesData = func.json_reader(filenameDBArt)
+    articleDF = pd.DataFrame(data=articlesData)
+
+    columnNames = ["tags"]
+    tags = pd.DataFrame(columns=columnNames)
+
+    for k, v in articleDF['tags'].items():
+        for tag in v:
+            tags = tags.append({'tags': tag}, ignore_index=True)
+    tagsWithCount = tags.groupby('tags', as_index=False)['tags'].size().to_frame('count').reset_index()
+    sortedTags = tagsWithCount.sort_values(by=['count'])[-7:]
+
+    sortedTags['tags'] = sortedTags['tags'].str.wrap(12)
+    sortedTags.plot.barh(x='tags', y='count', title='7 popular tags')
+
+    plt.tight_layout()
+    path = './src/parser/resources/img/tags.png'
+    plt.savefig(path)
+
+    return path
+
+def create_report(filenameDBArt, filenameDBAuth):
+    #  1) Top-5 Authors (based on articles counter)
+    #  2) Top-5 New Articles (based on publish data)
+    #  3) Plot with counts of 7 popular tags:
+    #  - it must be a bar chart (column plot) where each column is for one tag
+    #  - Tag bar must have name in plot.
+    #  - X-axis - counter with articles of tag theme
+    authorsReport = authors_report(filenameDBAuth)
+    articlesReport = articles_report(filenameDBArt)
+    tagsReport = tags_report(filenameDBArt)
+
+    authorsReport = json.loads(authorsReport.to_json(orient='records'))
+    articlesReport = json.loads(articlesReport.to_json(orient='records'))
+
+    report = {"authors": authorsReport, "articles": articlesReport, "tags": tagsReport}
+
+    return report
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
@@ -12,7 +74,11 @@ if __name__ == "__main__":
     crawler.do_crawler()
 
     logging.info('********* STEP 6. Do the report *********')
-    # TODO : do report
+    filenameDBArt = "./src/parser/resources/articles.json"
+    filenameDBAuth = "./src/parser/resources/authors.json"
+    report = create_report(filenameDBArt, filenameDBAuth)
 
     logging.info('********* STEP 7. Upload the report to the json-DB *********')
-    # TODO
+    func.json_writer("./src/parser/resources/report.json", report)
+
+    database.remover_temps()
